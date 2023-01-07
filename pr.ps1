dd02769f41c125cb292fa18d51aa3f95438b7df6
Humberto Massa <humbertomassa@gmail.com>
2023-01-07 12:12:21 -0300

    some changes

--- pr.ps1
+++ pr.ps1
@@ -1,3 +1,48 @@
+Set-StrictMode -Version Latest
+$ProgressPreference = 'SilentlyContinue'
+$ErrorActionPreference = 'Stop'
+trap {
+    Write-Output "ERROR: $_"
+    Write-Output (($_.ScriptStackTrace -split '\r?\n') -replace '^(.*)$','ERROR: $1')
+    Write-Output (($_.Exception.ToString() -split '\r?\n') -replace '^(.*)$','ERROR EXCEPTION: $1')
+    Exit 1
+}
+
+# enable TLS 1.2.
+[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol `
+    -bor [Net.SecurityProtocolType]::Tls12
+
+# wrap the choco command (to make sure this script aborts when it fails).
+function Start-Choco([string[]]$Arguments, [int[]]$SuccessExitCodes=@(0)) {
+    $command, $commandArguments = $Arguments
+    if ($command -eq 'install') {
+        $Arguments = @($command, '--no-progress') + $commandArguments
+    }
+    for ($n = 0; $n -lt 10; ++$n) {
+        if ($n) {
+            # NB sometimes choco fails with "The package was not found with the source(s) listed."
+            #    but normally its just really a transient "network" error.
+            Write-Host "Retrying choco install..."
+            Start-Sleep -Seconds 3
+        }
+        &C:\ProgramData\chocolatey\bin\choco.exe @Arguments
+        if ($SuccessExitCodes -Contains $LASTEXITCODE) {
+            return
+        }
+    }
+    throw "$(@('choco')+$Arguments | ConvertTo-Json -Compress) failed with exit code $LASTEXITCODE"
+}
+function choco {
+    Start-Choco $Args
+}
+
+# install winget
+# $ProgressPreference='Silent'
+Invoke-WebRequest -Uri https://github.com/microsoft/winget-cli/releases/download/v1.3.2691/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -OutFile .\MicrosoftDesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
+Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
+Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+
 # install adk
 $artifactUrl = 'https://download.microsoft.com/download/0/1/C/01CC78AA-B53B-4884-B7EA-74F2878AA79F/adk/adksetup.exe' # v1809
 $artifactPath = "$env:TEMP\$(Split-Path -Leaf $artifactUrl)"
